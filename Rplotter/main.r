82968c014fa77af1e60ff9579ba3ee14374036fd
ThePhong1803 <112880913+ThePhong1803@users.noreply.github.com>
2023-07-15 22:15:31 +0700

    update chart

--- Rplotter/main.r
+++ Rplotter/main.r
@@ -7,10 +7,10 @@ lines(dataframe2[,1], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "gree
 lines(dataframe2[,2], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "yellow")
 legend("topright", legend=c("RMSE_ReLU", "ACC_ReLU", "RMSE_Sigmoid", "ACC_Sigmoid"), col=c("blue", "red", "green", "yellow"), lty=1:2, cex=0.8)
 
-png(file="C:/Users/PC/Desktop/NN/Rplotter/MSE_ACC_600.png" ,width=1920, height=1080)
+png(file="C:/Users/PC/Desktop/NN/Rplotter/ReLU_Sigmoid.png" ,width=1920, height=1080)
 plot(dataframe1[,1], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "blue", ylim = c(0,1))
 lines(dataframe1[,2], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "red")
-lines(dataframe2[,2], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "green")
+lines(dataframe2[,1], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "green")
 lines(dataframe2[,2], type = 'l', xlab = "Epoch", ylab = "RMSE/ACC", col = "yellow")
 legend("topright", legend=c("RMSE_ReLU", "ACC_ReLU", "RMSE_Sigmoid", "ACC_Sigmoid"), col=c("blue", "red", "green", "yellow"), lty=1:2, cex=0.8)
 dev.off()
